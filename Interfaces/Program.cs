f798782e7e10ff05b09ee229766f0759cd022335
Zeynep Dursun <zeynep_dursun_2001@hotmail.com>
2021-01-27 18:09:52 +0300

    Engin Demiroğ Kampı

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -6,12 +6,68 @@ namespace Interfaces
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PersonManager manager = new PersonManager();
+            Customer customer = new Customer
+            { 
+                Id = 1, 
+                FirstName = "Engin", 
+                LastName = "Demiroğ", 
+                Address = "Ankara" 
+            };
+
+            Student student = new Student
+            {
+                Id = 1,
+                FirstName = "Derin",
+                LastName = "Demiroğ",
+                Department = "Computer Sciences"
+            };
+            manager.Add(customer);
+            manager.Add(student);
+            Console.ReadLine();
         }
     }
 
     interface IPerson
     {
+        int Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+    }
 
+    class Customer : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+    }
+
+    class Student : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Department { get; set; }
+    }
+    class Worker : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Department { get; set; }
+    }
+
+
+    class PersonManager
+    {
+        public void Add(IPerson person)
+        {
+            Console.WriteLine(person.FirstName);
+        }
     }
 }
